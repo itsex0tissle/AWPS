25137dadbdfaab617d12a8b8da73f015467bb987
Ілля Цап <its.experimental.version.0@outlook.com>
2026-03-19 12:09:25 +0200

    feat(iot): reworking mqtt interactor

---
+++ AWPS.IoT/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AWPS.IoT
+{
+    public static class Helper
+    {
+        public static bool Retry(Action action, int retry = 5)
+        {
+            for(; retry >= 0; retry--)
+            {
+                try
+                {
+                    action();
+                    return true;
+                }
+                catch { }
+            }
+            return false;
+        }
+    }
+}
--- AWPS.IoT/MqttInteraction/MqttInteractor.cs
+++ AWPS.IoT/MqttInteraction/MqttInteractor.cs
@@ -1,93 +1,98 @@
 ﻿using System;
 using System.Text;
 using System.Threading;
-using AWPS.IoT.Measuring;
 using System.Collections;
-using System.Diagnostics;
 using nanoFramework.M2Mqtt;
-using AWPS.IoT.BinaryRecords;
 using nanoFramework.M2Mqtt.Messages;
 using System.Security.Cryptography.X509Certificates;
 
 namespace AWPS.IoT.MqttInteraction
 {
-    public static class MqttInteractor
+    public sealed class MqttInteractor
     {
-        private static MqttClient CreateClient()
+        private MqttClient Mqtt { get; }
+
+        public MqttInteractor()
         {
             string server_url = MqttResources.GetString(MqttResources.StringResources.ServerUrl);
             X509Certificate cert = new(MqttResources.GetString(MqttResources.StringResources.ServerCertificate));
-            Debug.WriteLine("Create mqtt client");
-            MqttClient client = new(server_url, 8883, true, cert, null, MqttSslProtocols.TLSv1_2);
-            string client_id = Guid.NewGuid().ToString();
+            Mqtt = new MqttClient(server_url, 8883, true, cert, null, MqttSslProtocols.TLSv1_2);
+        }
+
+        public bool EnsureConnected(int retry = 5)
+        {
+            if(Mqtt.IsConnected is true)
+            {
+                return true;
+            }
             string username = MqttResources.GetString(MqttResources.StringResources.UserName);
             string password = MqttResources.GetString(MqttResources.StringResources.Password);
-            Debug.WriteLine("Connect to mqtt");
-            client.Connect(client_id, username, password, cleanSession: true, keepAlivePeriod: 60);
-            return client;
-        }
-        private static bool SendData(MqttClient client, byte[] buffer, string topic)
-        {
-            Debug.WriteLine($"Send data on '{topic}' topic");
-            string response_topic = $"{topic}/response";
-            bool response = false;
-            bool response_got = false;
-            Timer timer = new(state =>
+            return Helper.Retry(delegate()
             {
-                Debug.WriteLine($"Waiting for response on '{topic}' timeout");
+                string client_id = Guid.NewGuid().ToString();
+                MqttReasonCode code = Mqtt.Connect(client_id, username, password, cleanSession: true, keepAlivePeriod: 60);
+                if(code is not MqttReasonCode.Success)
+                {
+                    throw new Exception($"Failed to connect to mqtt: ReasonCode = {code}");
+                }
+            }, retry);
+        }
+        public byte[]? SendReceive(string send_topic, string receive_topic, byte[] buffer, int timeout = 10000, int retry = 5)
+        {
+            byte[]? response = null;
+            bool response_got = false;
+            Timer timer = new(delegate(object? state)
+            {
                 response_got = true;
-            }, null, 20000, Timeout.Infinite);
-            client.MqttMsgPublishReceived += WaitForConfirmResponse;
-            client.Subscribe(new string[] { response_topic }, new MqttQoSLevel[] { MqttQoSLevel.AtLeastOnce });
-            client.Publish($"{topic}/request", buffer, "", new ArrayList(), MqttQoSLevel.AtLeastOnce, false);
+            }, null, timeout, Timeout.Infinite);
+            Mqtt.MqttMsgPublishReceived += SaveMessage;
+            bool success = Helper.Retry(delegate()
+            {
+                if(EnsureConnected(retry) is false)
+                {
+                    throw new Exception("Can`t connect to MQTT");
+                }
+                Mqtt.Subscribe(new string[] { receive_topic }, new MqttQoSLevel[] { MqttQoSLevel.AtLeastOnce });
+                Mqtt.Publish(send_topic, buffer, "", new ArrayList(), MqttQoSLevel.AtLeastOnce, false);
+            }, retry);
+            if(success is false)
+            {
+                timer.Dispose();
+                return null;
+            }
             while(response_got is false)
             {
-                Thread.Sleep(500);
+                Thread.Sleep(100);
             }
             return response;
 
-            void WaitForConfirmResponse(object sender, MqttMsgPublishEventArgs event_args)
+            void SaveMessage(object sender, MqttMsgPublishEventArgs event_args)
             {
-                try
+                if(event_args.Topic != receive_topic)
                 {
-                    if(event_args.Topic == response_topic)
-                    {
-                        response = Encoding.UTF8.GetString(event_args.Message, 0, event_args.Message.Length) is "true";
-                        Debug.WriteLine($"Response got on {topic}: {response}");
-                    }
+                    return;
                 }
-                finally
-                {
-                    client.MqttMsgPublishReceived -= WaitForConfirmResponse;
-                    timer.Dispose();
-                    response_got = true;
-                }
+                response = event_args.Message;
+                response_got = true;
+                timer.Dispose();
             }
         }
-        public static void Start()
+        public bool SendConfirm(string send_topic, string receive_topic, byte[] buffer, int timeout = 10000, int retry = 5)
         {
-            for(int retry = 0; retry <= 5; retry++)
+            try
             {
-                try
+                if(SendReceive(send_topic, receive_topic, buffer, timeout, retry) is byte[] response)
                 {
-                    Debug.WriteLine("Start mqtt interactor");
-                    using MqttClient client = CreateClient();
-                    SendMeasuringDataFile(client);
-                    break;
+                    return Encoding.UTF8.GetString(response, 0, response.Length) is "true";
                 }
-                catch(Exception exc)
+                else
                 {
-                    Debug.WriteLine($"Mqtt interactor failed due to exception: {exc}");
+                    return false;
                 }
             }
-
-            static void SendMeasuringDataFile(MqttClient client)
+            catch
             {
-                MeasuringDataFile.Load();
-                if(SendData(client, BinaryRecord.SerializeEnumerable(MeasuringDataFile.Records), "measuring-data") is true)
-                {
-                    MeasuringDataFile.Reset();
-                }
+                return false;
             }
         }
     }
